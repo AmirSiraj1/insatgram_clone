f9a83a1d03c1f8b58513582be5e324346abddde1
Amir Siraj <amaressabit0314@gmail.com>
2023-02-28 14:40:08 +0300

    input fields

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:insatgram_clone/utils/colors.dart';
+import 'package:insatgram_clone/widgets/text_field_input.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,6 +11,16 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,9 +43,19 @@ class _LoginScreenState extends State<LoginScreen> {
             ),
             const SizedBox(
               height: 64,
-            )
+            ),
             // text field input for email
+            TextFieldInput(
+                textEditingController: _emailController,
+                hintText: 'Enter your Email',
+                textInputType: TextInputType.emailAddress),
             // text field input for password
+            TextFieldInput(
+              textEditingController: _emailController,
+              hintText: 'Enter your Password',
+              textInputType: TextInputType.text,
+              isPass: true,
+            ),
             // button login
             // Transition to signing up
           ],

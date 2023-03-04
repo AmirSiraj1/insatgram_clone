34f68e9eba2ac1c718207d100a2eafc40447c6cb
Amir Siraj <amaressabit0314@gmail.com>
2023-03-04 20:24:16 +0300

    addpost page

---
+++ lib/screens/add_post_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class AddPostScreen extends StatefulWidget {
+  const AddPostScreen({super.key});
+
+  @override
+  State<AddPostScreen> createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: IconButton(onPressed: () {}, icon: const Icon(Icons.upload)),
+    );
+  }
+}

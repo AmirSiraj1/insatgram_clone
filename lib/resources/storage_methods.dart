a736b0ca7502b0cf41a110131c8964a2870a40eb
Amir Siraj <amaressabit0314@gmail.com>
2023-03-09 22:39:03 +0300

    delete post

--- lib/resources/storage_methods.dart
+++ lib/resources/storage_methods.dart
@@ -12,11 +12,12 @@ class StorageMethods {
       String childName, Uint8List file, bool isPost) async {
     Reference ref =
         _storage.ref().child(childName).child(_auth.currentUser!.uid);
-    UploadTask uploadTask = ref.putData(file);
+
     if (isPost) {
       String id = const Uuid().v1();
       ref = ref.child(id);
     }
+    UploadTask uploadTask = ref.putData(file);
 
     TaskSnapshot snap = await uploadTask;
     String downloadUrl = await snap.ref.getDownloadURL();

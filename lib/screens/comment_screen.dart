44657021f16affb4470c36eb108d037fb39a4d51
Amir Siraj <amaressabit0314@gmail.com>
2023-03-09 20:53:21 +0300

    comment screen

--- lib/screens/comment_screen.dart
+++ lib/screens/comment_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:insatgram_clone/utils/colors.dart';
 
+import '../widgets/comment_card.dart';
+
 class CommentsScreen extends StatefulWidget {
   const CommentsScreen({super.key});
 
@@ -19,6 +21,41 @@ class _CommentsScreenState extends State<CommentsScreen> {
         centerTitle: false,
         title: const Text('Comments'),
       ),
+      body: const CommentCard(),
+      bottomNavigationBar: SafeArea(
+          child: Container(
+        height: kToolbarHeight,
+        margin: EdgeInsets.only(
+          bottom: MediaQuery.of(context).viewInsets.bottom,
+        ),
+        padding: const EdgeInsets.only(left: 16, right: 8),
+        child: Row(
+          children: [
+            const CircleAvatar(
+              radius: 18,
+              backgroundColor: Colors.amber,
+            ),
+            const Expanded(
+              child: Padding(
+                padding: EdgeInsets.only(left: 16, right: 8),
+                child: TextField(
+                  decoration: InputDecoration(hintText: 'comment as username '),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                child: const Text(
+                  'Post',
+                  style: TextStyle(color: Colors.blueAccent),
+                ),
+              ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }

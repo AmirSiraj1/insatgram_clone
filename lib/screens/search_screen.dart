f07b8cd69d0897ad9ef4927b33c9553ac7ff8faa
Amir Siraj <amaressabit0314@gmail.com>
2023-03-12 10:26:30 +0300

    search Screen page

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,8 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:insatgram_clone/utils/colors.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -14,6 +11,7 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   TextEditingController searchController = TextEditingController();
+  bool isShowUser = false;
   @override
   void dispose() {
     super.dispose();
@@ -23,38 +21,47 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: mobileBackgroundColor,
-        title: TextFormField(
-          controller: searchController,
-          decoration: const InputDecoration(labelText: 'Search For User'),
-          onFieldSubmitted: (value) {},
-        ),
-      ),
-      body: FutureBuilder(
-        future: FirebaseFirestore.instance
-            .collection('users')
-            .where('username', isGreaterThanOrEqualTo: searchController.text)
-            .get(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          return ListView.builder(
-            itemCount: (snapshot.data! as dynamic).docs.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      (snapshot.data! as dynamic).docs[index]['photoUrl']),
-                ),
-              );
+        appBar: AppBar(
+          backgroundColor: mobileBackgroundColor,
+          title: TextFormField(
+            controller: searchController,
+            decoration: const InputDecoration(labelText: 'Search For User'),
+            onFieldSubmitted: (value) {
+              setState(() {
+                isShowUser = true;
+              });
             },
-          );
-        },
-      ),
-    );
+          ),
+        ),
+        body: isShowUser
+            ? FutureBuilder(
+                future: FirebaseFirestore.instance
+                    .collection('users')
+                    .where('username',
+                        isGreaterThanOrEqualTo: searchController.text)
+                    .get(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return ListView.builder(
+                    itemCount: (snapshot.data! as dynamic).docs.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              (snapshot.data! as dynamic).docs[index]
+                                  ['photoUrl']),
+                        ),
+                        title: Text((snapshot.data! as dynamic).docs[index]
+                            ['username']),
+                      );
+                    },
+                  );
+                },
+              )
+            : const Text('post'));
   }
 }

1e603a80acf12c1487472a5ec61a9f71d769bdef
Amir Siraj <amaressabit0314@gmail.com>
2023-03-12 10:21:56 +0300

    search Screen page

---
+++ lib/screens/search_screen.dart
@@ -0,0 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:insatgram_clone/utils/colors.dart';
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({super.key});
+
+  @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController searchController = TextEditingController();
+  @override
+  void dispose() {
+    super.dispose();
+    searchController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: TextFormField(
+          controller: searchController,
+          decoration: const InputDecoration(labelText: 'Search For User'),
+          onFieldSubmitted: (value) {},
+        ),
+      ),
+      body: FutureBuilder(
+        future: FirebaseFirestore.instance
+            .collection('users')
+            .where('username', isGreaterThanOrEqualTo: searchController.text)
+            .get(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: (snapshot.data! as dynamic).docs.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      (snapshot.data! as dynamic).docs[index]['photoUrl']),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
